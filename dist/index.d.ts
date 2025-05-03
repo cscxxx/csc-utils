8ce1340c4e1df42b05406a5a03ebd3544f3ecf73
cscxxx <167551334+cscxxx@users.noreply.github.com>
2025-05-04 00:57:44 +0800

    移除误提交的dist目录

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import _ from "lodash";
-import dayjs from "dayjs";
-import axios from "axios";
-export { _, dayjs, axios };
-export declare function formatDate(date: Date | string, format?: string): string;
-export declare function debounce<T extends (...args: any[]) => any>(func: T, wait: number): (...args: Parameters<T>) => void;
-export declare function fetchData<T>(url: string): Promise<T>;
